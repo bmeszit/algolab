96fbd492da4ce8280d1ba4e38925b08f3f429e69
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2025-03-22 19:54:48 +0100

    Gepek

--- lab1/03-gepek/megoldas.cpp
+++ lab1/03-gepek/megoldas.cpp
@@ -5,11 +5,15 @@ using ll = long long;
 ll n, t;
 vector<ll> a;
 
+// Adott idő alatt legyártható-e t db termék?
 bool can_make(ll time)
 {
+  // Mennyi van még hátra.
   ll rem = t;
   for(int i=0; i<n; ++i)
   {
+    // Itt érdemes gyorsan visszatérni, különben ha
+    // a time nagy, könnyen túlcsordulhatunk.
     if(rem <= 0) break;
     rem -= time / a[i];
   }
@@ -21,10 +25,12 @@ int main()
   cin>>n>>t;
   a.assign(n, {}); for(auto& ax: a) cin>>ax;
 
+  // Bináris keresés a [lo, hi] intervallumon.
   ll lo = 0;
   ll hi = a[0]*t;
   while(lo < hi)
   {
+    // A (lo+hi)/2 túlcsordulhat nagy értékekre.
     ll mid = (hi-lo)/2+lo;
     if(can_make(mid)) hi = mid;
     else lo = mid+1;
