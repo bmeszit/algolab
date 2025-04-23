38d24d168a4fbb0c0461fd68ccf9552734525513
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2025-04-23 15:00:19 +0200

    add

--- lab4/hf1-boldogtalansag/megoldas.cpp
+++ lab4/hf1-boldogtalansag/megoldas.cpp
@@ -1,177 +1,100 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int infty = numeric_limits<int>::max();
+struct boy { vector<int> ord; int i; };
+struct girl { vector<int> rank; int i; };
 
 struct gale_shapely
 {
   int n, m;
-  vector<int> boy_idx;
-  vector<vector<int>> boy_props;
-  vector<map<int, int>> girl_prefs;
-
-  int regret;
-  vector<int> boy_best_idx;
-  vector<int> girl_best_idx;
-
-  queue<int> free_boys;
-  vector<int> girl_match;
-  vector<int> boy_match;
-
-  void init(int n, int m)
+  vector<boy> b; // Fiúk
+  vector<girl> g; // Lányok
+  
+  gale_shapely(int n, int m) : n(n), m(m)
   {
-    this->n=n;
-    this->m=m;
-    boy_idx.assign(n, {});
-    boy_props.assign(n, {});
-    girl_prefs.assign(m, {});
+    b.assign(n, { ord: {}, i: 0 });
+    g.assign(m, { rank: vector<int>(n, -1), i: -1 });
   }
 
-  void add_boy_pref(int b, int g, int p) { boy_props[b].push_back(g); }
-  void add_girl_pref(int g, int b, int p) { girl_prefs[g][b] = p; }
+  void b_ord_next(int bi, int gi) { b[bi].ord.push_back(gi); }
+  void g_rank(int gi, int bi, int p) { g[gi].rank[bi] = p; }
 
-  void reset()
+  int propose(int bi)
   {
-    free_boys = {};
-    for(int b=0; b<n; ++b) free_boys.push(b);
-    boy_match.assign(n, -1);
-    girl_match.assign(m, -1);
-  }
-
-  bool boy_has_next(int b)
-  {
-    return boy_idx[b] < boy_props[b].size();
-  }
-
-  int boy_next(int b)
-  {
-    return boy_props[b][boy_idx[b]++];
-  }
-
-  int girl_rank(int g, int b)
-  {
-    return girl_prefs[g].count(b) ? girl_prefs[g][b] : infty;
-  }
-
-  void propose(int b)
-  {
-    int g = boy_next(b);
-    int ob = girl_match[g];
-    if(ob != -1)
-    {
-      if(girl_rank(g, ob) < girl_rank(g, b)) { free_boys.push(b); return; }
-      boy_match[ob] = -1;
-      free_boys.push(ob);
-    }
-    girl_match[g] = b;
-    boy_match[b] = g;
+    boy& bx = b[bi];
+  
+    // Ha nincs felkérhető lány, nem próbálkozunk tovább.
+    if(bx.ord.size() <= bx.i) return -1;
+  
+    // Ha a következő lány nem ismeri a fiút, próbálkozunk tovább.
+    // Irányítatlan élekre ezt nem kell külön nézni.
+    if(g[bx.ord[bx.i]].rank[bi] == -1) { ++bx.i; return bi; }
+  
+    girl& gx = g[bx.ord[bx.i]];
+  
+    // Ha a lánynak nincs párja, akkor sikerült!
+    if(gx.i == -1) { gx.i = bi; return -1; }
+  
+    // Ha a lány mostani párja jobb, próbálkozunk tovább.
+    if(gx.rank[gx.i] < gx.rank[bi])  { ++bx.i; return bi; }
+  
+    // Egyébként a mostani párt elutasítja a lány.
+    int bo = gx.i; gx.i = bi;
+    ++b[bo].i; return bo;
   }
 
   void run()
   {
-    while(!free_boys.empty())
+    for(int i=0; i<n; ++i)
     {
-      int b = free_boys.front(); free_boys.pop();
-      if (!boy_has_next(b)) continue;
-      propose(b);
-    }
-  }
-
-  void print(bool rev=false)
-  {
-    int cnt=0;
-    for(int b=0; b<n; ++b) if(boy_match[b] != -1) ++cnt;
-    cout << cnt << endl;
-
-    if(rev)
-    {
-      for(int g=0; g<m; ++g)
-      {
-        if(girl_match[g] == -1) continue;
-        cout << g+1 << " " << girl_match[g]+1 << endl;
-      }
-    }
-    else
-    {
-      for(int b=0; b<n; ++b)
-      {
-        if(boy_match[b] == -1) continue;
-        cout << b+1 << " " << boy_match[b]+1 << endl;
-      }
+      int bi = i;
+      while(bi != -1) bi = propose(bi);
     }
   }
 };
 
+
+
 int main()
 {
-  int n, m;
-  cin>>n>>m;
-
-  // Lefuttatjuk a lánykérő algoritmust mindkét irányból.
-
-  gale_shapely boy, girl;
-  boy.init(n, m); boy.reset();
-  girl.init(m, n); girl.reset();
-
-  for(int b=0; b<n; ++b)
+  int n, m; cin>>n>>m;
+  gale_shapely gs(n, m), gs_rev(m, n);
+  
+  for(int i=0; i<n; ++i)
   {
-    int k; cin>>k; for(int p=0; p<k; ++p)
+    int k; cin>>k;
+    for(int j=0; j<k; ++j)
     {
-      int g; cin>>g; --g;
-      boy.add_boy_pref(b, g, p);
-      girl.add_girl_pref(b, g, p);
+      int x; cin>>x; --x;
+      gs_rev.g_rank(i, x, j);
+      gs.b_ord_next(i, x);
+    }
+  }
+  for(int i=0; i<m; ++i)
+  {
+    int k; cin>>k;
+    for(int j=0; j<k; ++j)
+    {
+      int x; cin>>x; --x;
+      gs.g_rank(i, x, j);
+      gs_rev.b_ord_next(i, x);
     }
   }
 
-  for(int g=0; g<m; ++g)
+  gs.run();
+  gs_rev.run();
+
+  for(int bi=0; bi<n; ++bi)
   {
-    int k; cin>>k; for(int p=0; p<k; ++p)
-    {
-      int b; cin>>b; --b;
-      boy.add_girl_pref(g, b, p);
-      girl.add_boy_pref(g, b, p);
-    }
+    auto& bx = gs.b[bi];
+    auto& bx_rev = gs_rev.g[bi];
+    cout << bx_rev.rank[bx_rev.i] - bx.i << endl;
   }
-
-  boy.run();
-  girl.run();
-
-  // Megnézzük ki hanyadik opcióját kapta a két esetben,
-  // ezek külbségei a boldogtalanságok.
-
-  vector<int> boy_regret(n, -1);
-  vector<int> girl_regret(m, -1);
-
-  for(int b=0; b<n; ++b)
+  for(int gi=0; gi<m; ++gi)
   {
-    int boy_best_match = boy.boy_match[b];
-    int boy_worst_match = girl.girl_match[b];
-
-    int rank_best = girl.girl_rank(b, boy_best_match);
-    int rank_worst = girl.girl_rank(b, boy_worst_match);
-
-    if(rank_best != infty) boy_regret[b] = rank_worst - rank_best;
+    auto& gx = gs.g[gi];
+    auto& gx_rev = gs_rev.b[gi];
+    cout << gx.rank[gx.i] - gx_rev.i << endl;
   }
-
-  for(int g=0; g<m; ++g)
-  {
-    int girl_best_match = girl.boy_match[g];
-    int girl_worst_match = boy.girl_match[g];
-
-    int rank_best = boy.girl_rank(g, girl_best_match);
-    int rank_worst = boy.girl_rank(g, girl_worst_match);
-
-    if(rank_best != infty) girl_regret[g] = rank_worst - rank_best;
-  }
-
-  for(int b=0; b<n; ++b)
-  {
-    cout << boy_regret[b] << endl;
-  }
-  for(int g=0; g<m; ++g)
-  {
-    cout << girl_regret[g] << endl;
-  }
-
   return 0;
 }
